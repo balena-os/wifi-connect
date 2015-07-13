f6ebd9694337698ea72abe6dc964195d083dce76
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-07-13 15:18:08 -0300

    Use ajax for form, better post-submit message

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -4,4 +4,12 @@ $(function(){
 			$("#ssid-select").append("<option value='" + val.ssid + "'>" + val.ssid + "</option>");
 		});		
 	})
+
+	$('#connect-form').submit(function(ev){
+		$.post('/connect', $('#connect-form').serialize(), function(data){
+			$('.before-submit').hide();
+			$('#submit-message').removeClass('hidden');
+		});
+		ev.preventDefault();
+	});
 });
