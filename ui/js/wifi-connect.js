c9fa9a3d5a4d34e05a8d02a3c23a6f44fdf3921c
majorz <zahari@resin.io>
2018-07-30 09:28:27 +0300

    Security settings

--- ui/js/wifi-connect.js
+++ ui/js/wifi-connect.js
@@ -1,4 +1,35 @@
+$('.chosen-select').chosen({disable_search_threshold: 10});
+
+$(document).on('change', '.custom-file-input', function () {
+	let fileName = $(this).val().replace(/\\/g, '/').replace(/.*\//, '');
+	$(this).parent('.custom-file').find('.custom-file-label').text(fileName);
+});
+
+$("#settings").hide();
+
 $(function(){
+
+	$('#wifi-networks-list a').on('click', function (e) {
+		e.preventDefault();
+
+		$("#wifi-networks").hide();
+		$("#settings").show();
+	})
+
+	$('#security-select').chosen().change(function (event) {
+		$("option:selected", this).tab('show');
+	});
+
+	$('#eap-authentication').chosen().change(function (event) {
+		$("option:selected", this).tab('show');
+
+		if($(this).val() === 'tls') {
+			$("#eap-username-password-pane").hide();
+		} else {
+			$("#eap-username-password-pane").show();
+		}
+	});
+
 	var networks = undefined;
 
 	function showHideEnterpriseSettings() {
@@ -20,7 +51,7 @@ $(function(){
 		ev.preventDefault();
 	});
 
-	$.get("/networks", function(data){
+	$.get('/networks', function(data){
 		if(data.length === 0){
 			$('.before-submit').hide();
 			$('#no-networks-message').removeClass('hidden');
