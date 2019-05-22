f69b481a12256f2e4419bce41710d969a897c7d5
MoriKeisuke <mori.keisuke@tis.co.jp>
2019-05-22 15:20:45 +0900

    fix(Delete printf): 直後の分岐条件がを参照していたためprintf文を除去

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,8 +16,6 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # 4. Is there an active WiFi connection?
 iwgetid -r
 
-printf 'Checking Changed Shell\n'
-
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
