6ef4fd46b5f269a76d25cc22b80f60a2bffc069d
MoriKeisuke <mori.keisuke@tis.co.jp>
2019-05-22 11:45:00 +0900

    feat(Delete command line argument): コマンドライン引数を除去

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,13 +16,13 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # 4. Is there an active WiFi connection?
 iwgetid -r
 
-printf 'Checking Changed Shell\n'
+printf 'Checking Changed Shell by Log\n'
 
 if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
     printf 'Starting WiFi Connect\n'
-    ./wifi-connect --portal-ssid "WiFi-Connect!"
+    ./wifi-connect
 fi
 
 # Start your application here.
