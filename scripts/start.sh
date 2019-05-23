992a4df77bc2b3cb62d849329b3ff8e685bdc00e
MoriKeisuke <mori.keisuke@tis.co.jp>
2019-05-23 09:56:39 +0900

    feat(Dynamic SSID): 環境変数を用いた動的SSIDの設定
    環境変数`DEVICE_NAME`および`BALENA_DEVICE_NAME_AT_INIT`を用いた動的SSIDを設定できる機能を追加した。
    以下疑似コードを示す。
    ```py
    if 環境変数${DEVICE_NAME}が存在する:
        return "WiFi-Connect_".format(DEVICE_NAME)
    elif 環境変数${BALENA_DEVICE_NAME_AT_INIT}が存在する:
        return "WiFi-Connect_".format(BALENA_DEVICE_NAME_AT_INIT)
    else:
        return "WiFi-Connect_sample"
    ```

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,10 @@
 
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
+# 環境変数DEVICE_NAMEが無ければ環境変数BALENA_DEVICE_NAME_AT_INITを文字列結合する
+# 環境変数BALENA_DEVICE_NAME_AT_INITが無ければ文字列"sample"を文字列結合する
+wifi_connect_portal_ssid="WiFi-Connect_${DEVICE_NAME:-${BALENA_DEVICE_NAME_AT_INIT:-sample}}"
+
 # Choose a condition for running WiFi Connect according to your use case:
 
 # 1. Is there a default gateway?
@@ -20,7 +24,7 @@ if [ $? -eq 0 ]; then
     printf 'Skipping WiFi Connect\n'
 else
     printf 'Starting WiFi Connect\n'
-    ./wifi-connect --portal-ssid "ほげほげまん" -p "password"
+    ./wifi-connect --portal-ssid "$wifi_connect_portal_ssid"
 fi
 
 # Start your application here.
