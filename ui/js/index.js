2d5b90ca78a88b91cb39e8afcf5a41f72ecd6679
MoriKeisuke <mori.keisuke@tis.co.jp>
2019-05-29 11:15:15 +0900

    docs(JSDoc): `ui/js/index.js` のJSDocを記述
    `ui/js/index.js` の各変数、各関数、および全体説明を記したJSDocを記述した。

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,6 +1,27 @@
+/**
+ * SSIDセレクトボックスおよび接続ボタンの振舞いを表す。
+ *
+ * <p> jQueryのreadyイベント関数として記述している。
+ * 事前にjQueryライブラリを読込む必要がある。
+ * Javascriptにおける即時関数ではない。
+ *
+ * <p> {@code ui/index.html} で利用している。
+ */
 $(function(){
+    /**
+     * Json形式で所有しているSSIDデータ
+     */
 	var networks = undefined;
 
+    /**
+     * WPA-Enterprise方式の場合はユーザ名テキストエリアを表示する。
+     *
+     * <p> SSIDセレクトボックスにおいて、WPA-Enterprise方式のSSIDを選択した場合、
+     * SSIDセレクトボックスとパスフレーズテキストエリアの間に、
+     * ユーザ名テキストエリアを表示する。
+     *
+     * <p> {@code #ssid-select} の振舞いで利用する。
+     */
 	function showHideEnterpriseSettings() {
 		var security = $(this).find(':selected').attr('data-security');
 		if(security === 'enterprise') {
@@ -10,8 +31,20 @@ $(function(){
 		}
 	}
 
+    /**
+     * SSIDセレクトボックスにおけるSSID選択時の動作を設定する。
+     */
 	$('#ssid-select').change(showHideEnterpriseSettings);
 
+    /**
+     * SSID選択肢リストを作成してセレクトボックスに格納する。
+     *
+     * <p> {@code <Gateway_IP_address>/network} 接続時のJson形式のレスポンスデータをパースし、
+     * SSID選択肢リストとして成型する。
+     * 成型したSSID選択肢リストを {@code #ssid-select} に格納する。
+     *
+     * <p> 格納する際に、 {@link #showHideEnterpriseSettings()} を呼出す。
+     */
 	$.get("/networks", function(data){
 		if(data.length === 0){
 			$('.before-submit').hide();
@@ -19,6 +52,7 @@ $(function(){
 		} else {
 			networks = JSON.parse(data);
 			$.each(networks, function(i, val){
+                // 空SSID（シークレットSSID）をリストから除去
                 if((val.ssid !== '') && ($('#ssid-select option[val="' + val.ssid + '"]').length === 0)){
                     $('#ssid-select').append(
                         $('<option>')
@@ -33,6 +67,9 @@ $(function(){
 		}
 	});
 
+    /**
+     * 接続ボタン押下時の挙動を設定する。
+     */
 	$('#connect-form').submit(function(ev){
 		$.post('/connect', $('#connect-form').serialize(), function(data){
 			$('.before-submit').hide();
