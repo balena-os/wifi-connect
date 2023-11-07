8b8b534657686f574213d1d973b953450d753840
thienandangthanh <thienandangthanh@gmail.com>
2023-11-07 16:09:03 +0700

    Replace old ui with new vite ui

--- ui-vite/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import 'whatwg-fetch';
-import 'promise-polyfill/src/polyfill';
-
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- ui/src/components/Notifications.tsx
+++ ui/src/components/Notifications.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { Txt, Alert } from 'rendition';
 
 export const Notifications = ({
