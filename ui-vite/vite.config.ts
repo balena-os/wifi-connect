eddbe992c26934e2af5ca2857ec6b41cfd8365e8
thienandangthanh <thienandangthanh@gmail.com>
2023-11-07 14:52:09 +0700

    Use `build` instead of `dist` as build output
    Reason: to match create-react-app output

--- ui-vite/vite.config.ts
+++ ui-vite/vite.config.ts
@@ -4,6 +4,9 @@ import browserslistToEsbuild from "browserslist-to-esbuild"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+      outDir: "build",
+    },
     plugins: [react()],
     target: browserslistToEsbuild(
         [
