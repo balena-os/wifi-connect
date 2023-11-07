bc3a29cbdbc6a538316712dde403fc357d68cea9
thienandangthanh <thienandangthanh@gmail.com>
2023-11-07 13:42:45 +0700

    Add browsersListToEsbuild config

--- ui-vite/vite.config.ts
+++ ui-vite/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import browserslistToEsbuild from "browserslist-to-esbuild"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    target: browserslistToEsbuild(
+        [
+            ">0.2%",
+            "not dead",
+            "not op_mini all"
+        ]
+    ),
 })
