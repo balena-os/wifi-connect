b7579d7456c08052eb3fe2679c1c8457ed922600
Carlos Andrés Álvarez Restrepo <charlie_cha@outlook.com>
2026-02-09 17:29:22 -0500

    Feature/branding (#1)
    * Fix dockerfile build
    
    * ustom robotcom brand
    
    * mske favicon smaller

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../img/logo.svg';
+import logo from '../img/logo.png';
 import { Navbar, Provider, Container } from 'rendition';
 import { NetworkInfoForm } from './NetworkInfoForm';
 import { Notifications } from './Notifications';
