d33df10f282b1dcfdaa85d0fa01c87039cdb3616
Carlos Alvarez <candres.alv@gmail.com>
2026-02-11 13:41:21 -0500

    Add timeout at startime to avoid launchgin AP mode if already wifi

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # sometimes. In this case, following checks will fail and wifi-connect
 # will be launched even if the device will be able to connect to a WiFi network.
 # If this is your case, you can wait for a while and then check for the connection.
-# sleep 15
+sleep 25
 
 # Choose a condition for running WiFi Connect according to your use case:
 
